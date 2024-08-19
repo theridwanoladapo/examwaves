1beea7e2881fe81716f8f4b23d005b3774b060f9
Ridwan Oladapo <olaitanoladapo29@gmail.com>
2024-08-19 14:08:21 +0100

    update homepage view

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $exams = \App\Models\Exam::limit(6)->get();
-    $certifications = \App\Models\Certification::limit(3)->get();
+    $certifications = \App\Models\Certification::limit(6)->get();
 
     return view('home', compact(['exams','certifications']));
 })
@@ -36,7 +36,7 @@ Route::middleware(['auth', 'admin-access'])
         ->name('exams.index');
     Route::get('exams/create', [ExamController::class, 'create'])
         ->name('exams.create');
-    
+
     Route::get('certifications', [CertificationController::class, 'index'])
         ->name('certifications.index');
     Route::get('certifications/create', [CertificationController::class, 'create'])
